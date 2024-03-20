af185ccb4f86f2f8608818f6a9898dd5461105de
norasyuhada <norasyuhada455@gmail.com>
2024-03-21 05:55:59 +0700

    tahap kedua

--- src/Duck.java
+++ src/Duck.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class Duck {
+interface Quackable {
+    void quack();
+}
+
+interface Flyable{
+    void fly();
+}
+
+abstract class Duck {
+    abstract void display();
+
+    void swim() {
+        System.out.println("Berenang");
+    }
+}
+
+class MallardDuck extends Duck implements Quackable, Flyable {
+    @Override
+    public void quack() {
+        System.out.println("Qwek Qwek");
+    }
+
+    public void fly(){
+        System.out.println("Terbang");
+    }
+    @Override
+    void display() {
+        System.out.println("Tampilan MalladrDuck");
+    }
+}
+
+class ReaheadDuck extends Duck implements Quackable, Flyable {
+    @Override
+    public void quack() {
+        System.out.println("Qwek Qwek");
+    }
+
+    public void fly(){
+        System.out.println("Terbang");
+    }
+
+    @Override
+    void display(){
+        System.out.println("Tampilan RedHeadDuck");
+    }
+}
+
+class RubberDuck extends Duck implements Quackable {
+    @Override
+    public void quack() {
+        System.out.println("Squeak");
+    }
+    @Override
+    void display() {
+        System.out.println("Tampilan RubberDuck");
+    }
+}
+
+class WoodenDuck extends Duck {
+    @Override
+    void display() {
+        System.out.println("Tampilan WoodenDuck");
+    }
+}
+
+class ToyDuck extends Duck implements Flyable {
+    public void fly() {
+        System.out.println("Terbang seperti mainan");
+    }
+
+
+    @Override
+    void display() {
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,74 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+
+
+
+            abstract class Duck{
+                void quack(){
+                    System.out.println("Qwek Qwek");
+                }
+                void swim(){
+                    System.out.println("Terbang");
+                }
+                abstract void display();
+
+                void fly(){
+                    System.out.println("Terbang");
+                }
+            }
+
+            class MallardDuck extends Duck {
+                @Override
+                void display(){
+                    System.out.println("Tampilan RedHeadDuck");
+                }
+
+            }
+
+            class RedHeadDuck extends Duck {
+                @Override
+                void display(){
+                    System.out.println("Tampilan RedHeadDuck");
+                }
+            }
+
+            class RubberDuck extends Duck {
+                @Override
+                void display(){
+                    System.out.println("Tampilan RubberDuck");
+                }
+                @Override
+                void quack(){
+                    System.out.println("Bunyikan squeek, bukan qwak");
+                }
+
+                @Override
+                void fly(){
+                    // Tidak ada implementasi, karena bebek mainan tidak dapat terbang
+                }
+            }
+
+            class WoodenDuck extends Duck {
+                @Override
+                void display(){
+                    System.out.println("Tampilan WoodenDuck");
+                }
+                @Override
+                void quack(){
+                    // Tidak ada implementasi, karena bebek mainan tidak dapat bersuara
+                }
+
+                @Override
+                void swim(){
+                    System.out.println("Mengambang");
+                }
+
+                @Override
+                void fly(){
+                    // Tidak ada implementasi, karena bebek mainan itu tidak dapat terbang
+                }
+            }
         }
     }
 }
