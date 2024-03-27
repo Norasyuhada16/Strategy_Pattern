1fa640ff360d0d08a6a18f2eec2750f29941675a
norasyuhada <norasyuhada455@gmail.com>
2024-03-27 14:48:39 +0700

    Tahap ketiga

--- src/Duck.java
+++ src/Duck.java
@@ -1,50 +1,46 @@
 interface Quackable {
     void quack();
 }
-
-interface Flyable{
+interface Flyable {
     void fly();
 }
-
-abstract class Duck {
+abstract class Duck{
     abstract void display();
 
     void swim() {
         System.out.println("Berenang");
     }
 }
-
-class MallardDuck extends Duck implements Quackable, Flyable {
+class MallarDuck extends Duck implements Quackable, Flyable {
     @Override
     public void quack() {
         System.out.println("Qwek Qwek");
     }
 
+    @Override
+    public void fly() {
+        System.out.println("Terbang");
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Tampilan MallardDuck");
+    }
+}
+class RedheadDuck extends Duck implements Quackable, Flyable {
+    @Override
+    public void quack() {
+        System.out.println("Qwek Qwek");
+    }
+    @Override
     public void fly(){
         System.out.println("Terbang");
     }
     @Override
     void display() {
-        System.out.println("Tampilan MalladrDuck");
-    }
-}
-
-class ReaheadDuck extends Duck implements Quackable, Flyable {
-    @Override
-    public void quack() {
-        System.out.println("Qwek Qwek");
-    }
-
-    public void fly(){
-        System.out.println("Terbang");
-    }
-
-    @Override
-    void display(){
         System.out.println("Tampilan RedHeadDuck");
     }
 }
-
 class RubberDuck extends Duck implements Quackable {
     @Override
     public void quack() {
@@ -55,22 +51,19 @@ class RubberDuck extends Duck implements Quackable {
         System.out.println("Tampilan RubberDuck");
     }
 }
-
 class WoodenDuck extends Duck {
     @Override
     void display() {
         System.out.println("Tampilan WoodenDuck");
     }
 }
-
 class ToyDuck extends Duck implements Flyable {
+    @Override
     public void fly() {
         System.out.println("Terbang seperti mainan");
     }
-
-
     @Override
     void display() {
-
+        System.out.println("Tampilan ToyDuck");
     }
 }
